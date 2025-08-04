f8f2be131bbe0940562c239723698e634a8ab81e
Ankit-69k <ankitkumarray19@gmail.com>
2025-08-04 19:28:20 +0530

    add landing page and course search components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Footer from "@/components/footer";
 import NonDashboardNavbar from "@/components/nonDashboardNavbar";
 import Image from "next/image";
+import Landing from "./(nondashboard)/landing/page";
 
 export default function Home() {
   return (
     <div className="nondashboard-layout">
       <NonDashboardNavbar />
-      <main className="nondashboard-layout__main">{/* <Landing /> */}</main>
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>
       <Footer />
     </div>
   );
